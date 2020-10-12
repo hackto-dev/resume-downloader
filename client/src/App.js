4982aafe7083e8c571ddc5889ac5bbf7abdbef48
hackto-dev <pratham.kulkarni1999@gmail.com>
2020-10-13 01:29:34 +0530

    Removed node_modules and added to gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 import MyPDF from './files/Resume.pdf';
-
+import Draggable from 'react-draggable';
 
 class App extends Component {
   render() {
     return (
-        <div className="container">
+      <div className="container">
+        <Draggable onDrag={this.handleEvent}>
           <div className="button-container">
             <a href={MyPDF} download="Resume.pdf">
               <button class="btn">
-                <span>Open Document</span>
+                <span>Download Document</span>
               </button>
             </a>
           </div>
-        </div>
+        </Draggable>
+      </div>
     );
   }
 }

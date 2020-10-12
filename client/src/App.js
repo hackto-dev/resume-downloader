963669ce581212b74806f13a408c43fa5a5f163a
hackto-dev <pratham.kulkarni1999@gmail.com>
2020-10-13 01:19:07 +0530

    Removed backend. Not required. Working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import { Router, Route } from 'react-router-dom';
 import './App.css';
+import MyPDF from './files/Resume.pdf';
+
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <div className='button-container'>
-          <Link className='btn'>
-            <span>Open Document</span>
-          </Link>
+        <div className="container">
+          <div className="button-container">
+            <a href={MyPDF} download="Resume.pdf">
+              <button class="btn">
+                <span>Open Document</span>
+              </button>
+            </a>
+          </div>
         </div>
-      </div>
-    )
+    );
   }
 }
 
